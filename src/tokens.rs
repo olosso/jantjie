cd3bcec7d66d700671b9367e80abab57d1c8a1a6
olosso <sorsa.olli@gmail.com>
2022-07-30 14:38:36 +0300

    Add tokens.

---
+++ src/tokens.rs
@@ -0,0 +1,83 @@
+pub enum TokenType {
+    Illegal,
+    EOF,
+
+    // Identifiers + literals
+    Ident(String),
+    Int(i64),
+
+    // Operators
+    Assign,
+    Plus,
+
+    // Delimiters
+    Comma,
+    Semicolon,
+
+    Lparen,
+    Rparen,
+    Lbrace,
+    Rbrace,
+
+    // Keywords
+    Function,
+    Let,
+}
+
+impl TokenType {
+    fn string(&self) -> String {
+        let s = match self {
+            TokenType::Illegal => "ILLEGAL",
+            TokenType::EOF => "EOF",
+
+            TokenType::Ident(_) => "IDENT",
+            TokenType::Int(_) => "INT",
+
+            TokenType::Assign => "=",
+            TokenType::Plus => "+",
+
+            TokenType::Comma => ",",
+            TokenType::Semicolon => ";",
+
+            TokenType::Lparen => "(",
+            TokenType::Rparen => ")",
+            TokenType::Lbrace => "{",
+            TokenType::Rbrace => "}",
+
+            TokenType::Function => "FUNCTION",
+            TokenType::Let => "LET",
+        };
+
+        String::from(s)
+    }
+
+    fn from(s: &str) -> Self {
+        match s {
+            "=" => TokenType::Assign,
+            "+" => TokenType::Plus,
+
+            "," => TokenType::Comma,
+            ";" => TokenType::Semicolon,
+
+            "(" => TokenType::Lparen,
+            ")" => TokenType::Rparen,
+            "{" => TokenType::Lbrace,
+            "}" => TokenType::Rbrace,
+
+            "fn" => TokenType::Function,
+            "let" => TokenType::Let,
+
+            "INT" => TokenType::Int(s.parse().unwrap()),
+            "IDENT" => TokenType::Ident(String::from(s)),
+
+            _ => TokenType::Illegal,
+        }
+    }
+}
+
+#[cfg(test)]
+mod token_tests {
+
+    #[test]
+    fn hello() {}
+}
